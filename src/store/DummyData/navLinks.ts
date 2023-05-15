0f2082ba111642e6836fb54ebdfd0ca1aef45757
Arachalev <ekongedet01@gmail.com>
2023-05-15 19:04:01 +0100

    modified config

--- src/store/DummyData/navLinks.ts
+++ src/store/DummyData/navLinks.ts
@@ -1,9 +1,9 @@
 export const navLinksData = [
-  { name: "New Arrivals", href: "arrivals" },
-  { name: "Top Ranking", href: "rankings" },
-  { name: "Sell on TBT", href: "sell" },
-  { name: "Buy on TBT", href: "buy" },
-  { name: "Buyer Leads", href: "leads" },
-  { name: "About Us", href: "about" },
+  { name: "New Arrivals", href: "new-arrivals" },
+  { name: "Top Ranking", href: "top-rankings" },
+  { name: "Sell on TBT", href: "sell-on-tbt" },
+  { name: "Buy on TBT", href: "buy-on-tbt" },
+  { name: "Buyer Leads", href: "request-for-quotation-seller" },
+  { name: "About Us", href: "about-us" },
   { name: "Help", href: "help" },
 ];
