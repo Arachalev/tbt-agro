b0616d47784164520efa8041dfe4fe6536e26e6f
Arachalev <ekongedet01@gmail.com>
2023-05-15 19:06:41 +0100

    modified routing

--- src/store/DummyData/navLinks.ts
+++ src/store/DummyData/navLinks.ts
@@ -1,9 +1,9 @@
 export const navLinksData = [
-  { name: "New Arrivals", href: "new-arrivals" },
-  { name: "Top Ranking", href: "top-rankings" },
-  { name: "Sell on TBT", href: "sell-on-tbt" },
-  { name: "Buy on TBT", href: "buy-on-tbt" },
-  { name: "Buyer Leads", href: "request-for-quotation-seller" },
-  { name: "About Us", href: "about-us" },
+  { name: "New Arrivals", href: "web/new-arrivals" },
+  { name: "Top Ranking", href: "web/top-rankings" },
+  { name: "Sell on TBT", href: "web/sell-on-tbt" },
+  { name: "Buy on TBT", href: "web/buy-on-tbt" },
+  { name: "Buyer Leads", href: "web/request-for-quotation-seller" },
+  { name: "About Us", href: "web/about-us" },
   { name: "Help", href: "help" },
 ];
